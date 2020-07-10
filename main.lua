1c551c0d74a76e1b20830a5c682047fa7173414d
ElderTwig <pqfrml@gmail.com>
2020-07-10 22:13:38 +0200

    Capital letters for all libraries

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 AnimLib = require("lib/animation")
-bump = require("lib/bump/bump")
-cron = require("lib/cron/cron")
+Bump = require("lib/bump/bump")
+Cron = require("lib/cron/cron")
 MGL = require("lib/MGL/src/MGL")
 
 G_Framerate = 0
@@ -9,7 +9,7 @@ G_ScreenWidth = 640
 G_ScreenHeight = 480
 
 function love.load()
-	G_screenCanvas = love.graphics.newCanvas(640,480)
+    G_screenCanvas = love.graphics.newCanvas(640,480)
 
     love.window.setMode(G_ScreenWidth, G_ScreenHeight, {vsync=-1, resizable=true})
     love.graphics.setBackgroundColor(19/255, 20/255, 68/255)
@@ -22,25 +22,25 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.setCanvas(G_screenCanvas)
-	love.graphics.clear(0,0,0,1)
+    love.graphics.setCanvas(G_screenCanvas)
+    love.graphics.clear(0,0,0,1)
 
-	--SetCameraPosition(love, -Camera_x, -Camera_y)
+    --SetCameraPosition(love, -Camera_x, -Camera_y)
 
-	love.graphics.setCanvas()
-	
+    love.graphics.setCanvas()
+    
     -- This resets the translation above
     -- so that we can draw GUI in screen space coordinates
     love.graphics.origin()
 
-	local windowwidth, windowheight = love.graphics.getDimensions()
+    local windowwidth, windowheight = love.graphics.getDimensions()
 
-	love.graphics.draw(G_screenCanvas, windowwidth/2-G_ScreenWidth/2, windowheight/2-G_ScreenHeight/2)
+    love.graphics.draw(G_screenCanvas, windowwidth/2-G_ScreenWidth/2, windowheight/2-G_ScreenHeight/2)
 
     -- This resets the translation above
     -- so that we can draw GUI in screen space coordinates
     love.graphics.origin()
-	love.graphics.translate(windowwidth/2-G_ScreenWidth/2, windowheight/2-G_ScreenHeight/2) 
+    love.graphics.translate(windowwidth/2-G_ScreenWidth/2, windowheight/2-G_ScreenHeight/2) 
 
     -- Coloured text, love.graphics.printf is available for formatted text	
     -- To round a number x with a precision delta_x do:
